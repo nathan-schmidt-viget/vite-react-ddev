2ad6285a33117d29b9f52063edc16d6263b7dc69
Joshua Pease <joshua.pease@viget.com>
2022-10-18 16:38:10 -0700

    Gets Vite working

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  root: 'src',
+  base: command === 'serve' ? '' : '/dist/',
+  build: {
+    // output dir for production build
+    outDir: '../dist',
+    emptyOutDir: true,
+    // emit manifest so PHP can find the hashed files
+    manifest: true,
+    // our entry
+    rollupOptions: {
+      input: './src/main.tsx',
+    }
+  },
 
   server: {
     host: "0.0.0.0",
+    origin: 'http://vite-project.ddev.site:5273',
     // we need a strict port to match on PHP side
     strictPort: true,
     // Vite port is defined by https://github.com/torenware/ddev-viteserve
-    port: 5173,
+    port: parseInt(process.env.VITE_PRIMARY_PORT ?? '5173'),
   },
-})
+}))
